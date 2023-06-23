5b837aef8f0d0c787aadaf3ceccb7e7829b54335
YoTi <yasserrafai5@gmail.com>
2023-06-23 18:10:21 +0100

    make a lot of changes again

--- factors.py
+++ factors.py
@@ -1,23 +1,52 @@
 #!/usr/bin/python3
-#import pdb; pdb.set_trace()
-import math
 import sys
+import subprocess
+import time
+import resource
+from math import isqrt
 
+def factorize_number(n):
+    """
+    Factorizes a given number into a product of two smaller numbers.
+    Returns the factors if found, None otherwise.
+    """
+    for i in range(2, isqrt(n) + 1):
+        if n % i == 0:
+            return i, n // i
+    return None
 
-filename = sys.argv[1]
+def factorize_file(filename):
+    """
+    Factorizes numbers from a given file.
+    Prints the factorization for each number found.
+    """
+    with open(filename, 'r') as file:
+        for line in file:
+            number = int(line.strip())
+            factors = factorize_number(number)
+            if factors is not None:
+                p, q = factors
+                print(f"{number}={p}*{q}")
+            else:
+                print(f"{number} is a prime number.")
 
-with open(filename, 'r', encoding='utf-8') as f:
-    a = f.read()
-    a_1 = a.count("\n")
-lineas = 0
-b = 0
-with open(filename, 'r', encoding='utf-8') as f:
-    while lineas < a_1:
-        n1 = int (f.readline())
-        lineas += 1
-        a = 2
-        while a < n1//2:
-            if math.fmod(n1, a) == 0:
-                print("{:d} = {:d} * {:d}".format(n1, a, int(n1/a), end=""))
-                a = n1
-            a += 1
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python factors.py <file>")
+    else:
+        file_path = sys.argv[1]
+
+        start_time = time.time()
+        start_user_time, start_sys_time = resource.getrusage(resource.RUSAGE_SELF).ru_utime, resource.getrusage(resource.RUSAGE_SELF).ru_stime
+
+        # Print the factorization results
+        factorize_file(file_path)
+
+        elapsed_time = time.time() - start_time
+        elapsed_user_time = resource.getrusage(resource.RUSAGE_SELF).ru_utime - start_user_time
+        elapsed_sys_time = resource.getrusage(resource.RUSAGE_SELF).ru_stime - start_sys_time
+
+        # Print the time output
+        print(f"real\t{elapsed_time:.3f}s")
+        print(f"user\t{elapsed_user_time:.3f}s")
+        print(f"sys\t{elapsed_sys_time:.3f}s")
