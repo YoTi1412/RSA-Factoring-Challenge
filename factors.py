a919fc2ee2e7c453d105e949f683c7c6ff3ff530
YoTi <yasserrafai5@gmail.com>
2023-06-23 16:51:40 +0100

    again

--- factors.py
+++ factors.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 import signal
-from sys import argv
+import sys
 from math import sqrt
 
 
@@ -8,32 +8,36 @@ def factor():
     def timeout_handler(signum, frame):
         raise TimeoutError("Program execution exceeded time limit")
 
-    # Set the signal handler for the alarm
     signal.signal(signal.SIGALRM, timeout_handler)
-    # Set the alarm for 5 seconds
     signal.alarm(5)
 
     try:
-        with open(argv[1]) as f:
+        filename = sys.argv[1]
+        with open(filename) as f:
             for line in f:
                 num = int(line)
                 print("{:d}=".format(num), end="")
                 if num % 2 == 0:
-                    print("{}*2".format(num//2))
+                    print("{}*2".format(num // 2))
                     continue
                 sqn = int(sqrt(num))
                 if sqn % 2 == 0:
                     sqn += 1
                 for i in range(3, sqn + 1, 2):
                     if num % i == 0:
-                        print("{}*{}".format(i, num//i))
+                        print("{}*{}".format(i, num // i))
                         break
                 if num % i != 0:
                     print("{}={}*1".format(num, num))
     except TimeoutError as e:
         print("Error: Program execution exceeded time limit")
+    except IndexError:
+        print("Error: No input file provided")
+    except FileNotFoundError:
+        print("Error: File not found")
+    except Exception as e:
+        print("Error:", str(e))
     finally:
-        # Reset the alarm
         signal.alarm(0)
 
 
